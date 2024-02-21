ec9dfaaa09d2c2e67140837f0c96178f9ae082f2
Alex <sanchezaleko001@gmail.com>
2024-02-21 10:23:42 +0300

    edits

--- school/student/urls.py
+++ school/student/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about),
-    path('location/', views.location),
-    path('fee-structure', views.fee_structure)
+    path('', views.about, name='home'),
+    path('location/', views.location, name='location'),
+    path('fee-structure', views.fee_structure, name='fee-structure')
 ]
